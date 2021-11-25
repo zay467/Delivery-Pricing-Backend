5e1f7f3c5c60aa7e2c21a2da60d97ae823fab8f8
Zay Min Maw <zayminmaw77@gmail.com>
2021-11-25 21:55:48 +0630

    added morgan logger

--- src/routers/courier.ts
+++ src/routers/courier.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import controller from "../controllers/courier";
 import extractJWT from "../middlewares/extractJWT";
+import morgan from "morgan";
 
 const router: Router = express.Router();
 
-router.get("/", controller.read);
-router.post("/", extractJWT, controller.create);
-router.put("/:id", extractJWT, controller.update);
-router.delete("/:id", extractJWT, controller.deleteItem);
+router.get("/", morgan("tiny"), controller.read);
+router.post("/", morgan("tiny"), extractJWT, controller.create);
+router.put("/:id", morgan("tiny"), extractJWT, controller.update);
+router.delete("/:id", morgan("tiny"), extractJWT, controller.deleteItem);
 
 export default router;
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import controller from "../controllers/user";
+import morgan from "morgan";
 
 const router: Router = express.Router();
 
-router.post("/register", controller.register);
-router.post("/login", controller.login);
+router.post("/register", morgan("tiny"), controller.register);
+router.post("/login", morgan("tiny"), controller.login);
 
 export default router;
