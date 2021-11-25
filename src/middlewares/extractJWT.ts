e5644665e4378271e2f520a59c6ddb81f6323643
Zay Min Maw <zayminmaw77@gmail.com>
2021-11-25 12:24:59 +0630

    added auth middleware

---
+++ src/middlewares/extractJWT.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+import { Request, Response, NextFunction } from "express";
+
+const extractJWT = (req: Request, res: Response, next: NextFunction) => {
+  let token = req.headers.authorization?.split(" ")[1];
+  if (token) {
+    jwt.verify(token, config.server.token.secret, (error, decoded) => {
+      if (error) {
+        return res.status(404).json({
+          message: error,
+          error,
+        });
+      } else {
+        res.locals.jwt = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
+
+export default extractJWT;
